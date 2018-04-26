97d81afdde6ff83ce6782102a9194153a0fab851
Phillzz <phillzzzz@gmail.com>
2018-04-26 09:55:51 +0300

    Small upd Bombs

--- Source/BombermanClone/Bomb.cpp
+++ Source/BombermanClone/Bomb.cpp
@@ -49,34 +49,45 @@ void ABomb::CheckForVictims()
 	FCollisionQueryParams TraceParams = FCollisionQueryParams(FName(TEXT("TraceParams")), false, this);
 
 	FVector Location = GetActorLocation();
-	GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X - DefaultBombRange, Location.Y, Location.Z), ObjectList, TraceParams);
-	GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X + DefaultBombRange, Location.Y, Location.Z), ObjectList, TraceParams);
-	GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X, Location.Y - DefaultBombRange, Location.Z), ObjectList, TraceParams);
-	GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X, Location.Y + DefaultBombRange, Location.Z), ObjectList, TraceParams);
+		GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X - DefaultBombRange, Location.Y, Location.Z), ObjectList, TraceParams);
+		GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X + DefaultBombRange, Location.Y, Location.Z), ObjectList, TraceParams);
+		GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X, Location.Y - DefaultBombRange, Location.Z), ObjectList, TraceParams);
+		GetWorld()->LineTraceMultiByObjectType(HitResults, GetRichLineStart(), FVector(Location.X, Location.Y + DefaultBombRange, Location.Z), ObjectList, TraceParams);
 	
 	for (int i = 0; i < HitResults.Num(); ++i)
 	{
 		HitData = HitResults[i];
-		if (HitData.GetActor()->IsA(APawn::StaticClass()) && APawn::StaticClass()->ImplementsInterface(UGameInterraction::StaticClass()))
+		AActor* FoundActor = HitData.GetActor();
+		//Check For if hit results not empty
+		if (!FoundActor)
 		{
-			//TODO: Send interface message destroy victim
-			UE_LOG(LogTemp, Warning, TEXT("Bomb Detect Pawn"));
+			UE_LOG(LogTemp, Error, TEXT("Hit Results Faild!"));
+			return;
+		}
+		//Send Message to pawn
+		if (FoundActor->IsA(APawn::StaticClass()))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Bomb: Pawn Detect"));
+			/*
+			if (FoundActor->GetClass()->ImplementsInterface(UGameInterraction::StaticClass()))
+				IGameInterraction::Execute_destroyBombVictim(FoundActor);
+			*/
 		}
 		if (HitData.GetActor()->IsA(ABomb::StaticClass())) //&& ABomb::StaticClass()->ImplementsInterface(InteractionLogic::StaticClass()))
 		{
 			//TODO: Send interface message destroy victim
-			UE_LOG(LogTemp, Warning, TEXT("Bomb Detect Bomb"));
+			UE_LOG(LogTemp, Warning, TEXT("Bomb: Bomb Detect"));
 
 		}
 		if (HitData.GetActor()->IsA(ADestructable::StaticClass())) // && ADestructable::StaticClass()->ImplementsInterface(UBombermanInteractionInterface::StaticClass()))
 		{
 			//TODO: Send interface message destroy victim
-			UE_LOG(LogTemp, Warning, TEXT("Bomb Detect Destructable"));
+			UE_LOG(LogTemp, Warning, TEXT("Bomb: Destructable Detect"));
 		}
 		if (HitData.GetActor()->IsA(APickups::StaticClass())) // && APickups::StaticClass()->ImplementsInterface(UBombermanInteractionInterface::StaticClass()))
 		{
 			//TODO: Send interface message destroy victim
-			UE_LOG(LogTemp, Warning, TEXT("Bomb Detect Pickup"));
+			UE_LOG(LogTemp, Warning, TEXT("Bomb: Pickup Detect"));
 		}
 	}
 }
